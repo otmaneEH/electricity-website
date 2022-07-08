f2610c800acea0edc29f12e1ce23cefbc7a67b75
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-07-08 16:59:23 +0100

    commite

---
+++ www.skaelec.com/wp-content/themes/windfall/inc/dynamic-style.php
@@ -0,0 +1,165 @@
+  .no-class {}
+  .wndfal-preloader, .wndfal-back-top a, .wndfal-callout, .wndfal-form input[type="submit"],.wndfal-footer input[type="submit"],.more-btn:hover, .more-btn:focus, .appointment-style-two .appointment-form, .bullet-list li:before, .widget-contact-info .wndfal-icon, .table thead th, .testimonials-style-two .owl-carousel button.owl-dot.active, .testimonials-style-three .owl-carousel button.owl-dot.active,.woocommerce .cart_totals .calculate-shipping input[type="submit"], .woocommerce .cart_totals .shipping-calculator-form button.button, blockquote:before, .wndfal-partners .owl-carousel button.owl-dot.active, .btn-text-wrap, .masonry-filters ul li a:after, .wndfal-navigation ul li.open > .dropdown-arrow, .callout-style-two .wndfal-btn:hover,.callout-style-two .wndfal-btn:focus,.owl-carousel button.owl-dot.active, .more-btn:hover, .more-btn:focus, .wndfal-intro, .services-style-three .service-item, .services-request input[type="submit"]:hover, .services-request input[type="submit"]:focus, .faq-form input[type="submit"]:hover, .faq-form input[type="submit"]:focus, .contact-form form input[type="submit"]:hover, .contact-form form input[type="submit"]:focus, .woocommerce .price_slider_amount button.button:hover, .woocommerce #respond input#submit.alt:hover, .woocommerce a.button.alt:hover, .woocommerce button.button.alt:hover, .woocommerce input.button.alt:hover,.woocommerce #review_form #respond .form-submit input:hover, .woocommerce #respond input#submit:hover, .woocommerce #review_form #respond .form-submit input:focus, .woocommerce #respond input#submit:focus, .woocommerce form .form-row input.button:hover, .woocommerce form .form-row input.button:focus, .woocommerce form .form-row button.button:hover, .woocommerce form .form-row button.button:focus,.wndfal-comment-form input[type="submit"]:hover, .wndfal-comment-form input[type="submit"]:focus,.woocommerce .cart .actions input[type="submit"].update-cart:hover, .woocommerce .cart .actions input[type="submit"].update-cart:focus,.woocommerce .widget_price_filter .price_slider_amount button.button:hover, .woocommerce .widget_price_filter .price_slider_amount button.button:focus, .woocommerce a.button:hover, .woocommerce button.button:hover, .wndfal-form .wndfal-comment-form input[type="submit"]:hover, .wndfal-form .wndfal-comment-form input[type="submit"]:focus, .comment-respond input[type="submit"]:hover, .comment-respond input[type="submit"]:focus {
+    background-color: #ff6600;
+  }
+
+  ::selection {background: #ff6600;}
+  ::-webkit-selection {background: #ff6600;}
+  ::-moz-selection {background: #ff6600;}
+  ::-o-selection {background: #ff6600;}
+  ::-ms-selection {background: #ff6600;}
+
+  .header-contact-info a:hover, .header-contact-info a:focus, .search-link a:hover, .search-link a:focus,.industry-title a:hover, .industry-title a:focus, .check-list li:before, .wndfal-link:hover, .wndfal-link:focus, .stats-item [class*="ti-"], .emergency-title a:hover, .emergency-title a:focus, .blog-date i, .footer-wrap a:hover, .footer-wrap a:focus, .wndfal-copyright a:hover, .call-link a:hover, .call-link a:focus, .industry-info .wndfal-link, .feature-item .wndfal-icon, .author-name a:hover, .services-emergency span i, .gallery-info a:hover, .gallery-info a:focus, .emergency-call-link a span, .breadcrumb a:hover, .author-rating i.active, .wndfal-faq .section-title-wrap a:hover, .faq-meta a:hover, .contact-info ul li [class*="ti-"], .contact-info ul li i, .contact-inner-info a:hover, .wndfal-widget ul li a:hover, .wndfal-widget ul li a:focus, .woocommerce .cart_totals .calculate-shipping a:hover, .woocommerce form .lost_password a:hover, .post-date i, .blog-date ul li a:hover, .wndfal-blog-tags a:hover, .woocommerce .star-rating span, .wndfal-navigation > ul > li.active > a, .breadcrumbs ul.trail-items li a:hover, .wndfal-widget.widget_rss ul li a:hover, .wndfal-widget.widget_recent_comments ul li a:hover, .woocommerce p.stars.selected a.active:before, .woocommerce p.stars:hover a:before, .woocommerce p.stars.selected a:not(.active)::before, .vt-nav-links > div:hover, .vt-nav-links > div:hover a, .footer-widget .header-contact-info a:hover, .header-style-one .wndfal-navigation > ul > li.current-menu-ancestor > a, .header-style-one .wndfal-navigation > ul > li.current-menu-parent > a {
+    color: #ff6600;
+  }
+
+  .woocommerce-product-search button:hover, .woocommerce .woocommerce-widget-layered-nav-dropdown__submit:hover, .testi-global .owl-carousel button.owl-dot.active, .saspot-social.rounded a:hover, .woocommerce .cart .actions button.button:hover, .woocommerce-account .addresses .title .edit:hover {
+    background: #ff6600;
+  }
+
+  .more-btn:hover, .more-btn:focus {border-color: #ff6600;}
+
+  .no-class {}
+  .wndfal-back-top a:hover, .wndfal-back-top a:focus, .service-item.wndfal-hover:after, .wndfal-form input[type="submit"]:hover, .wndfal-form input[type="submit"]:focus, .wndfal-footer input[type="submit"]:hover, .wndfal-footer input[type="submit"]:focus, .wndfal-emergency, .appointment-style-two .appointment-form input[type="submit"], .wndfal-callout.callout-style-two, .mate-designation, .services-emergency span, .navigation-wrap, .service-details .bullet-list li:before, .services-request input[type="submit"], .widget-services .btn-link, .faq-form input[type="submit"], .contact-form form input[type="submit"], .woocommerce ul.products li.product.wndfal-hover .button, .woocommerce ul.products li.product.wndfal-hover .added_to_cart, .woocommerce nav.woocommerce-pagination ul li a:focus, .woocommerce nav.woocommerce-pagination ul li a:hover, .woocommerce nav.woocommerce-pagination ul li span.current, .woocommerce .widget_price_filter .price_slider_wrapper .ui-slider-horizontal .ui-slider-range, .tag-widget a:hover, .tag-widget a:focus, .tagcloud a:hover, .tagcloud a:focus,.woocommerce #respond input#submit.alt, .woocommerce a.button.alt, .woocommerce button.button.alt, .woocommerce input.button.alt, .woocommerce #review_form #respond .form-submit input, .woocommerce #respond input#submit, .woocommerce .cart .actions .coupon input[type="submit"]:hover, .woocommerce .cart .actions .coupon input[type="submit"]:focus, .woocommerce .cart .actions input[type="submit"].update-cart, .woocommerce .cart_totals .calculate-shipping input[type="submit"]:hover, .woocommerce .cart_totals .calculate-shipping input[type="submit"]:focus,.woocommerce .cart_totals .shipping-calculator-form button.button:hover,.woocommerce .cart_totals .shipping-calculator-form button.button:focus,.woocommerce form .form-row input.button, .page-item.active .page-link, .woocommerce form .form-row button.button, .page-link:hover, .page-link:focus, .blog-detail .bullet-list li:before, .wndfal-social.square a:hover, .wndfal-social.square a:focus, .wndfal-comments-area a.comment-reply-link:hover, .wndfal-comments-area .comment-reply-link:focus, .wndfal-comment-form input[type="submit"], .comment-respond input[type="submit"] .wndfal-social.rounded a:before, .wndfal-social.rounded a:after, .blog-comment a:hover, .blog-comment a:focus, .widget_search form input[type="submit"]:hover, .widget_search form input[type="submit"]:focus, .download-item.wndfal-hover .wndfal-icon, .wndfal-btn:before, .wndfal-btn:after, .wndfal-btn .btn-text-wrap:before, .wndfal-btn .btn-text-wrap:after, .radio-icon-wrap input[type="radio"]:checked + .radio-icon:before, .woocommerce span.onsale, .wndfal-navigation ul li .dropdown-nav li.open > .dropdown-arrow, .woocommerce a.button, .woocommerce button.button {
+    background-color: #034170;
+  }
+
+  .woocommerce-product-search button, .woocommerce .woocommerce-widget-layered-nav-dropdown__submit, .woocommerce .cart .actions input[type="submit"].update-cart, .woocommerce .cart .actions button.button, .woocommerce-account .addresses .title .edit, .owl-carousel .customer-name a:before, .owl-carousel .customer-name a:after {
+    background: #034170;
+  }
+
+  a:hover, a:focus, .wndfal-btn:hover, .wndfal-social a:hover, .wndfal-social a:focus, .dropdown-nav > li:hover > a, .dropdown-nav > li:focus > a, .dropdown-nav > li.active > a, .dropdown-nav > li.sub .dropdown-nav li:hover a, .btn-link, .btn-link:hover, .btn-link:focus, .wndfal-link, .call-link a, .header-style-two .wndfal-navigation > ul > li.active > a, .header-style-two .wndfal-navigation > ul > li.has-dropdown.active > a .menu-text:after, .mate-info ul li a:hover, .download-item .wndfal-icon, .header-contact-info a, .testimonials-style-two .author-name, .reliable-style-two .wndfal-border-btn, .widget-services .bullet-list li.active a, .widget-services .bullet-list li a:hover, .testimonials-style-two .author-name, .testimonials-style-three .author-name, .wndfal-faq .section-title-wrap a, .faq-meta a, .woocommerce div.product .product_meta a:hover, .woocommerce #reviews #comments ol.commentlist li time, .woocommerce .cart_totals .calculate-shipping a, .error-title, .wndfal-error .wndfal-btn, .author-content .wndfal-social a:hover, .wndfal-comments-area .wndfal-comments-meta .comments-date, ul.mate-contact, ul.mate-contact li a, .dropdown-nav > li.current-menu-ancestor > a, .dropdown-nav > li.current-menu-parent > a {
+    color: #034170;
+  }
+
+  .woocommerce nav.woocommerce-pagination ul li a:focus, .woocommerce nav.woocommerce-pagination ul li a:hover, .woocommerce nav.woocommerce-pagination ul li span.current, .wndfal-comments-area .comment-reply-link:hover, .wndfal-comments-area .comment-reply-link:focus {
+    border-color: #034170;
+  }
+.no-class {}
+.wndfal-topbar {
+  background-color: ;
+}
+.wndfal-topbar {
+  border-color: ;
+}
+@media (max-width: 1199px) {
+  .no-class {}
+  .search-link {
+    text-align: right;
+    position: relative;
+    z-index: 4;
+    top: 3px;
+  }
+  .header-style-two .header-btn {
+    margin-right: 45px;
+    position: relative;
+    z-index: 4;
+  }
+  .wndfal-main-wrap[class*="header-style-"] .wndfal-navigation .container {
+    padding: 0 15px;
+  }
+  .wndfal-main-wrap[class*="header-style-"] .wndfal-navigation > ul > li > a {
+    padding-left: 0;
+  }
+  .wndfal-toggle {
+    display: inline-block;
+  }
+  .navigation-wrap {
+    padding: 13px 20px;
+    position: relative;
+  }
+  .wndfal-navigation {
+    display: none !important;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 50vh;
+    padding-bottom: 25px;
+    margin: 0 auto;
+    background: #ffffff;
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+    -ms-overflow-style: -ms-autohiding-scrollbar;
+    -webkit-box-shadow: 0 3px 4px rgba(0, 0, 0, 0.2);
+    -ms-box-shadow: 0 3px 4px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 3px 4px rgba(0, 0, 0, 0.2);
+    z-index: 2;
+  }
+  .wndfal-navigation .container {
+    padding: 0;
+  }
+  .wndfal-navigation > ul > li {
+    float: none;
+  }
+  .wndfal-navigation > ul > li > a {
+    display: block;
+    padding: 10px 60px 10px 20px;
+    color: #777777;
+    line-height: 22px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  }
+  .wndfal-navigation ul > li.has-dropdown:hover > .dropdown-nav {
+    -webkit-transform: none;
+    -ms-transform: none;
+    transform: none;
+  }
+  .dropdown-nav {
+    display: none;
+    padding: 0 0 0 25px;
+    min-width: 10px;
+    opacity: 1;
+    visibility: visible;
+    position: static;
+    -webkit-transition: none;
+    -ms-transition: none;
+    transition: none;
+    -webkit-transform: none;
+    -ms-transform: none;
+    transform: none;
+    -webkit-box-shadow: none;
+    -ms-box-shadow: none;
+    box-shadow: none;
+  }
+  .wndfal-main-wrap[class*="header-style-"] .dropdown-nav {
+    padding-left: 15px;
+  }
+  .wndfal-navigation .has-dropdown.sub .dropdown-nav {
+    padding-left: 15px;
+  }
+  .dropdown-nav li a {
+    padding: 10px 60px 10px 0;
+    line-height: 22px;
+    border-bottom: 1px solid #dfdfdf;
+  }
+  .wndfal-navigation .has-dropdown.sub > a:after {
+    display: none;
+  }
+  .dropdown-nav > li:hover > a,
+  .dropdown-nav > li:focus > a,
+  .dropdown-nav > li.active > a {
+    background: transparent;
+  }
+  .wndfal-navigation > ul > li.has-dropdown > a .menu-text:after,
+  .wndfal-navigation > ul > li > a:after {
+    display: none;
+  }
+  .dropdown-arrow {
+    display: inline-block;
+  }
+
+}
+body, form p, .footer-wrap p,.woocommerce .woocommerce-result-count, .woocommerce-page .woocommerce-result-count,.woocommerce #reviews #comments ol.commentlist li .comment-text .meta, blockquote p, .author-content .author-name, .customer-name, .author-name, .services-emergency span, .blog-comment a,#multi-step-form .fw-wizard-buttons button, #multi-step-form .fw-wizard-buttons .fw-btn, .woocommerce #review_form #respond .comment-form label  {
+   font-family: "Muli";  }
+.wndfal-navigation, .wndfal-navigation > ul > li, .wndfal-navigation > ul > li > a, .mean-container .mean-nav ul li a  {
+   font-family: "Muli";}
+.dropdown-nav > li, .dropdown-nav li a, .dropdown-nav, .mean-container .mean-nav ul.sub-menu li a  {
+   font-family: "Muli";}
+h1, h2, h3, h4, h5, h6, .text-logo  {
+   font-family: "Muli";}
+.caption-subtitle, .check-list,.mate-info ul, .service-details .bullet-list, .table td, .accordion-style-two .bullet-list, .woocommerce ul.products li.product .price, .tag-widget a, .tagcloud a, .woocommerce #reviews #comments ol.commentlist li time, .woocommerce table.shop_table td .woocommerce-Price-amount, .woocommerce form .form-text, .woocommerce .woocommerce-checkout-review-order table.shop_table .cart_item,
+.woocommerce #add_payment_method #payment div.payment_box p, .woocommerce .woocommerce-cart #payment div.payment_box p, .woocommerce .woocommerce-checkout #payment div.payment_box p, .post-date, .blog-inner-title, .blog-detail .bullet-list, .wndfal-blog-tags, .wndfal-comments-area .wndfal-comments-meta .comments-date, .woocommerce span.onsale, .feature-item p,.section-title-wrap p,.blog-info p,
+.blog-detail p,.wndfal-blog-detail p.logged-in-as,.woocommerce p,.contact-info .section-title-wrap p,.card-body p,.widget-testimonials-info p,
+.brochure-wrap p,.about-info p,.industry-info-inner p,.service-item p,.card-body,.customer-item p,.service-info p,.testimonial-item p,.offer-info p, .download-info p  {
+   font-family: "Source Sans Pro"; font-weight:     normal;}
